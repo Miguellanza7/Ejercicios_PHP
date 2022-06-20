a766aed9cd0d44ea918c67554c6bf32a73e059ad
Miguel Lanza <104147479+Miguellanza7@users.noreply.github.com>
2022-06-20 00:11:11 -0600

    V1

---
+++ EjerciciosPHP/Ejercicios.php
@@ -0,0 +1,27 @@
+<?php
+echo ("EJERCICIOS DE PHP <br>");
+echo "<br> Suma: <br>";
+$a = 10;
+$b = 2; 
+echo "La suma de $a y $b es: <br>" . ($a + $b);
+
+echo "<br><br> Resta: <br>";
+$a = 10;
+$b = 2;
+echo "La resta de $a y $b es: " . ($a - $b);
+
+echo "<br><br>Multiplicación: <br>";
+$a = 10;
+$b = 2;
+echo "La multiplicación de $a y $b es: " . ($a * $b);
+
+echo "<br><br>División: <br>";
+$a = 10;
+$b = 2;
+echo "La división de $a y $b es: " . ($a / $b);
+
+echo "<br>Potencia: <br>";
+$a = 10;
+
+echo "La potencia de $a es: " . ($a ** 2);
+
